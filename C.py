525e36212b6792d42cb2bad2dff9283a93765321
Guntherswarchzhaffenstrassen <boris@localhost.localdomain>
2016-04-20 13:14:05 +0300

    The 3 task

--- C.py
+++ C.py
@@ -1,4 +1,27 @@
 f = open('input.txt')
+
+N = int(f.readline())
+mind = N
+A = list(map(int, f.readline().split()))
+
+f.close()
+
+for i in range(N):
+    if A[i] < 0:
+        for j in range(i + 1, N):
+            if A[i] + A[j] == 0 and j - i < mind:
+                mind = j - i
+
+f = open('output.txt', 'w')
+if mind == N:
+    print(0, file=f)
+else:
+    print(mind, file=f)
+
+f.close()
+
+'''
+f = open('input.txt')
 A = list(map(int, f.readline().split()))
 f.close()
 
@@ -17,3 +40,4 @@ for i in range(N):
 f = open('output.txt', 'w')
 print(0 if min_dist == N else min_dist, file=f)
 f.close()
+'''
