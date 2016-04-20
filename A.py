709daa831771435c193df5d62eeee86fca1841aa
Guntherswarchzhaffenstrassen <boris@localhost.localdomain>
2016-04-20 11:10:37 +0300

    First task to be tested

--- A.py
+++ A.py
@@ -1,9 +1,11 @@
-N = int(input())
-A = list(map(int,input().split()))
-A = A[:N]
+input = open('input.txt', 'r')
+output = open('output.txt', 'w')
+A = list(map(int, input.readline().split()))
 
 A.sort()
 for i in range(len(A)):
 	if A[i] == A[i+1]:
 		print(A[i])
 		break
+
+print(A[i], file=output)
