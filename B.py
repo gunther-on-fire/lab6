a863dcbaf9762f91773981a9748fa28979707af4
Guntherswarchzhaffenstrassen <boris@localhost.localdomain>
2016-04-20 13:08:02 +0300

    Solving the second task

--- B.py
+++ B.py
@@ -1,3 +1,4 @@
+'''
 fin = open('input.txt')
 N = int(fin.readline())
 sales = [int(nominal) for nominal in fin.readline().split()]
@@ -16,9 +17,10 @@ fout = open('output.txt', 'w')
 print(coins_needed, file = fout)
 fout.close()
 '''
-f = open('input.txt')
-A = list(map(int, f.readline().split()))
-f.close()
+fin = open('input.txt')
+N = int(fin.readline())
+A = list(map(int, fin.readline().split()))
+fin.close()
 
 min_coins_number = coins_number = 0
 
@@ -30,10 +32,9 @@ for elem in A:
 	if coins_number < min_coins_number:
 		min_coins_number = coins_number
 
-f = open('output.txt', 'w')
+fout = open('output.txt', 'w')
 if min_coins_number < 0:
-	print(abs(min_coins_number), file=f)
+	print(-min_coins_number, file=fout)
 else:
-	print(0, file=f)
-f.close()
-'''
+	print(0, file=fout)
+fout.close()
