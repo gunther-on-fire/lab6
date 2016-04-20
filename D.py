3a76a657359ba423e42016f3a8aeb6ae49848f6b
Guntherswarchzhaffenstrassen <boris@localhost.localdomain>
2016-04-20 14:32:57 +0300

    What's wrong?

--- D.py
+++ D.py
@@ -1,3 +1,22 @@
+
+f = open('input.txt')
+
+K, N = map(int, f.readline().split())
+M = list(map(int, f.readline().split()))
+
+for i in range(K - 1):
+    A = list(map(int, f.readline().split()))
+    for j in range(N):
+        M[j] = min(M[j], A[j])
+
+f.close()
+
+f = open('output.txt', 'w')
+
+print(" ".join(map(str, M)), file=f)
+
+f.close()
+'''
 f = open('input.txt')
 k, N = map(int, f.readline().split())
 
@@ -13,3 +32,4 @@ f.close()
 f = open('output.txt', 'w')
 print(B, file=f)
 f.close()
+'''
