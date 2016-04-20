b4dc68cf03515e2e3ffb67b165259ef30bab4d05
Guntherswarchzhaffenstrassen <boris@localhost.localdomain>
2016-04-20 12:06:03 +0300

    Changed A.py

--- A.py
+++ A.py
@@ -3,8 +3,8 @@ output = open('output.txt', 'w')
 A = list(map(int, input.readline().split()))
 
 A.sort()
-for i in range(len(A)):
-	if A[i+1] == A[i]:
+for i in range(len(A)-1):
+	if A[i] == A[i+1]:
 		print(A[i])
 		break
 
