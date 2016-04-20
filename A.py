1c561f7fc530a82322314c022005feaa03e86485
Guntherswarchzhaffenstrassen <boris@localhost.localdomain>
2016-04-20 12:30:18 +0300

    Another try for A.py

--- A.py
+++ A.py
@@ -1,11 +1,14 @@
-f = open('input.txt', 'r')
-A = list(map(int, input.readline().split()))
+f = open('input.txt')
+
+f.readline()
+A = list(map(int, f.readline().split()))
 f.close()
 
 A.sort()
 i = 1
 while A[i] != A[i - 1]:
-	i += 1
+    i += 1
+
 
 f = open('output.txt', 'w')
 print(A[i], file=f)
