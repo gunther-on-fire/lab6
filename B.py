3196be35e34d0be6f2c76bd6d6dc54580ebf7ead
Guntherswarchzhaffenstrassen <boris@localhost.localdomain>
2016-04-20 12:54:12 +0300

    123

--- B.py
+++ B.py
@@ -1,3 +1,21 @@
+fin = open('input.txt')
+N = int(fin.readline())
+sales = [int(nominal) for nominal in fin.readline().split()]
+
+counter = 0
+coins_needed = 0
+for sale in sales:
+    if sale == 5:
+        counter -= 1
+    else:
+        counter += sale//5 - 1
+    if counter > coins_needed:
+        coins_needed = counter
+
+fout = open('output.txt', 'w')
+print(coins_needed, file = fout)
+fout.close()
+'''
 f = open('input.txt')
 A = list(map(int, f.readline().split()))
 f.close()
@@ -18,3 +36,4 @@ if min_coins_number < 0:
 else:
 	print(0, file=f)
 f.close()
+'''
